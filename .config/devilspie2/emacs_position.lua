eb265bf9b1d64cbd52717dea95cd22643afb1f0b
tbizetde@gmail.com <tbizetde@gmail.com>
2025-01-01 17:24:07 -0300

    tmux yank to system clipboard

--- .config/devilspie2/emacs_position.lua
+++ .config/devilspie2/emacs_position.lua
@@ -1,5 +1,4 @@
 if (get_application_name() == "Emacs") then
     set_window_workspace(4)
-    set_window_position(2430, 50)
-    set_window_size(900, 900)
+    set_window_position(2430, 75)
 end
--- .config/devilspie2/kitty_position.lua
+++ .config/devilspie2/kitty_position.lua
@@ -1,4 +1,4 @@
 if (get_application_name() == "kitty") then
     set_window_position(1980, 0)
-    set_window_size(1000, 700)
+    set_window_size(1000, 600)
 end
