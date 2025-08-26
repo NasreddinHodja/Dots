508d77433c5d4aad5c26634c181d3a362db0117d
tbizetde@gmail.com <tbizetde@gmail.com>
2025-08-26 01:55:41 -0300

    BIG ASS HOUSEKEEPING

--- .config/devilspie2/emacs_position.lua
+++
@@ -1,4 +0,0 @@
-if (get_application_name() == "Emacs") then
-    set_window_workspace(4)
-    set_window_position(2430, 75)
-end
--- .config/devilspie2/kitty_position.lua
+++
@@ -1,4 +0,0 @@
-if (get_application_name() == "kitty") then
-    set_window_position(1980, 0)
-    set_window_size(900, 600)
-end
