5f04343a81ec696837ae9d409b7967f09367f852
tbizetde@gmail.com <tbizetde@gmail.com>
2025-11-26 08:45:16 -0300

    removed gaming layouts; CED layer

--- .config/naskbd/draw.sh
+++ .config/naskbd/draw.sh
@@ -2,11 +2,10 @@
 
 keymap draw -o diagram.svg keymap.yaml
 keymap draw -s BASE -o base.svg keymap.yaml
+keymap draw -s CED -o ced.svg keymap.yaml
 keymap draw -s SYM -o sym.svg keymap.yaml
 keymap draw -s NAV -o nav.svg keymap.yaml
 keymap draw -s MOU -o mou.svg keymap.yaml
 keymap draw -s FUN -o fun.svg keymap.yaml
-keymap draw -s GAM -o gam.svg keymap.yaml
-keymap draw -s GNM -o gnm.svg keymap.yaml
 
 sed -i '/class="layer-.*"/,/\/g/ {/class="label"/d}' *.svg
