c310867f3987513b8ed9906f59818ddfa7a143d7
tbizetde@gmail.com <tbizetde@gmail.com>
2025-12-24 11:59:58 -0300

    updates

--- .config/hypr/cycle-windows.sh
+++ .config/hypr/cycle-windows.sh
@@ -4,11 +4,11 @@ ACTIVE="$(hyprctl activewindow -j)"
 
 if echo "$ACTIVE" | jq -e '.grouped | length > 1' >/dev/null; then
     hyprctl dispatch changegroupactive f
-    notify-send "Hyprland cycle" "Group → next window"
+    # notify-send "Hyprland cycle" "Group → next window"
 elif echo "$ACTIVE" | jq -e '.floating == true' >/dev/null; then
     hyprctl dispatch cyclenext floating
-    notify-send "Hyprland cycle" "Floating → next window"
+    # notify-send "Hyprland cycle" "Floating → next window"
 else
     hyprctl dispatch cyclenext tiled
-    notify-send "Hyprland cycle" "Tiled → next window"
+    # notify-send "Hyprland cycle" "Tiled → next window"
 fi
