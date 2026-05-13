8041a6901765f4b48abfaa45a194d1d245966428
tbizetde@gmail.com <tbizetde@gmail.com>
2026-05-13 16:20:44 -0300

    better waybar

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -2,30 +2,12 @@
 
 set +e
 
-dbus-update-activation-environment --systemd --all
-# some env can't auto run the portal, so need this
-/usr/lib/xdg-desktop-portal-wlr  >/dev/null 2>&1 &
+dbus-update-activation-environment --systemd --all &
 
-# notify
-# swaync -c ~/.config/mango/swaync/config.jsonc -s ~/.config/mango/swaync/style.css >/dev/null 2>&1 &
 dunst >/dev/null 2>&1 &
-
-# wallpaper
 awww-daemon >/dev/null 2>&1 &
-
-# bar
 /home/nasreddin/Prog/Waybar/build/waybar >/dev/null 2>&1 &
-
-# bluetooth
 blueman-applet >/dev/null 2>&1 &
-
-# network
 nm-applet >/dev/null 2>&1 &
-
-# screeshot
 flameshot >/dev/null 2>&1 &
-# Permission authentication
-# /usr/lib/xfce-polkit/xfce-polkit >/dev/null 2>&1 &
-
-# main emacs
 /home/nasreddin/.local/bin/main-emacsclient >/dev/null 2>&1 &
